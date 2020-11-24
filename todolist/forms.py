ccf39a1e1d4d35be0823b7ce33ccda13a00e9f0b
swalih <swalih@qburst.com>
2020-11-24 12:31:09 +0530

    Added the form functinality

---
+++ todolist/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class TodoListForm(forms.Form):
+    text = forms.CharField(max_length=45,
+        widget=forms.TextInput(
+            attrs={'class' : 'form-control', 'placeholder' : 'Enter todo', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}
+        ))
